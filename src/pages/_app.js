9e409bd64b46b6f7646c27ca93a245a916c30153
Alejandro Chavez <alejandrochavez1202@gmail.com>
2023-03-31 15:43:59 -0400

    Setting up dev env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
-import '@styles/globals.css';
-
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
+  // Providers
+  // Context
+  // Layout
+  // Additional props
   return <Component {...pageProps} />;
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,20 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <title>Garage Store</title>
+        <meta charSet="utf-8" />
+        {/* favicon */}
+        {/* webfonts */}
+        {/* stylesheet */}
+        {/* scripts/js */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
